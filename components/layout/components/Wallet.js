09986af7120abce163007a80eeffc34571ef8bb0
pandit <dhamdherepandit@gmail.com>
2023-01-21 23:02:37 +0530

    fixing errors

--- components/layout/components/Wallet.js
+++ components/layout/components/Wallet.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { ethers } from 'ethers';
 import { useState } from "react";
+
 const networks = {
     goerli: {
         chainId: `0x${Number(5).toString(16)}`,
@@ -12,8 +13,8 @@ const networks = {
         },
         rpcUrls: ["https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
         blockExplorerUrls: ["https://goerli.etherscan.io"]
-    }
-}
+    },
+};
 
 
 const Wallet = () => {
@@ -23,14 +24,14 @@ const Wallet = () => {
     const connectWallet = async () => {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         // const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
 
         if (provider.network !== "eth") {
             await window.ethereum.request({
                 method: "wallet_addEthereumChain",
                 params: [
                     {
-                        ...networks["goerli"]
+                        ...networks["ethereum"]
                     }
                 ]
             })
