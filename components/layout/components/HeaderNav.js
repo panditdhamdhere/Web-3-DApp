da975253fd28be6dc4f0e905262c0c9543f079cd
pandit <dhamdherepandit@gmail.com>
2023-01-22 12:44:57 +0530

    added router

--- components/layout/components/HeaderNav.js
+++ components/layout/components/HeaderNav.js
@@ -1,17 +1,28 @@
 import styled from "styled-components"
+import { useRouter } from "next/router"
+import Link from "next/link";
+
 
 const HeaderNav = () => {
+  const Router = useRouter();
   return (
     <HeaderNavWrapper>
-<HeaderNavLinks>
-  Campaigns
-</HeaderNavLinks>
-<HeaderNavLinks>
-  Create Campaign
-</HeaderNavLinks>
-<HeaderNavLinks>
-  Dashboard
-</HeaderNavLinks>
+      <Link href={"/"}>
+        <HeaderNavLinks active={Router.pathname == "/" ? true : false}>
+          Campaigns
+        </HeaderNavLinks>
+      </Link>
+
+      <Link href={"/createcampaign"}>
+        <HeaderNavLinks active={Router.pathname == "/createcampaign" ? true : false}>
+          Create Campaign
+        </HeaderNavLinks>
+      </Link>
+      <Link href={"/dashboard"}>
+        <HeaderNavLinks active={Router.pathname == "/dashboard" ? true : false}>
+          Dashboard
+        </HeaderNavLinks>
+      </Link>
     </HeaderNavWrapper>
   )
 }
@@ -30,7 +41,7 @@ const HeaderNavLinks = styled.div`
 display: flex;
 align-items: center;
 justify-content: space-between;
-background-color: ${(props) => props.theme.bgSubDiv};
+background-color: ${(props) => props.active ? props.theme.bgSubDiv : props.theme.bgDiv};
 height: 100%;
 font-family: 'IBM Plex Mono';
 margin: 7px;
@@ -40,6 +51,5 @@ cursor: pointer;
 text-transform: uppercase;
 font-weight: bold;
 font-size: small;
-
 `
 export default HeaderNav
